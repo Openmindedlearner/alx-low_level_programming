4274ac67f58e69e85e8351d5a015b6dca9dbdda3
Dennis Njagi <gichdennis@gmail.com>
2022-09-15 16:19:39 -0700

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,18 +3,19 @@
 /**
  * print_alphabet_x10 - function to print alphabet 10 times
  *
- * Return: Always 0 (Sucess)
  */
 
-void print_alphabet_x10.c(void)
+void print_alphabet_x10(void)
 {
 	int line, ch;
 	
 	for (line = 0; line <= 9; ++line)
 	{
 		for (ch = 'a'; ch <= 'z'; ++ch)
+		{
 			_putchar(ch);
-		_putchar('n\');
+		}
+		_putchar('\n');
 	}
 }
 
