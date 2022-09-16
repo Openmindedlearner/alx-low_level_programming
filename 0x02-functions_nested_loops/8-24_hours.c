f59cb1f99d1c255a7da7e1722aa6496a6685db04
Dennis Njagi <gichdennis@gmail.com>
2022-09-15 18:43:13 -0700

    print every minute of a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,15 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for (hr = 0; min <= 23; ++hr)
+	for (hr = 0; hr <= 24; hr++)
 	{
-		for (min = 0; min <= 59; ++min)
+		for (min = 0; min <= 60; min++)
 		{
-			_putchar((hr / 10) + 48);
-			_putchar((hr % 10) + 48);
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + 48);
-			_putchar((min % 10) + 48);
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 	}
